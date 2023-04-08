ff58abad59f297932f46bdb6e4faca65c7bf7bbe
kursad.yildirim <kursad.yildirim@outlook.com>
2023-04-08 23:22:54 +0200

    minor fix

--- src/gdk/rest.go
+++ src/gdk/rest.go
@@ -2,10 +2,15 @@ package gdk
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func GenerateGrid(w http.ResponseWriter, r *http.Request) {
+	if err := ReadEnvironment(); err != nil {
+		fmt.Println(err)
+		return
+	}
 	err := json.NewDecoder(r.Body).Decode(&Props)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
